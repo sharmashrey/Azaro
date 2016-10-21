188f653b9b93b860402a8622ef18aad03d593290
Shreyash <shreyasdsharma@gmail.com>
2016-10-21 01:25:04 -0700

    Added new Activity Pages

--- app/src/main/java/shreyas/io/weld/azaro/MainActivity.java
+++ app/src/main/java/shreyas/io/weld/azaro/MainActivity.java
@@ -1,5 +1,6 @@
 package shreyas.io.weld.azaro;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -80,8 +81,11 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_subjects) {
             // Handle the camera action
+            Intent intent = new Intent( MainActivity.this, SubjectActivity.class);
+            MainActivity.this.startActivity(intent);
+
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
